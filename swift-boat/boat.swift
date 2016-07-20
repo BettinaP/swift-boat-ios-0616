bbb2c7c73298f28f76e30c6223197f0a64b7ee12
BettinaP <bettinaprophete@gmail.com>
2016-07-19 20:55:33 -0400

    done

---
+++ swift-boat/boat.swift
@@ -0,0 +1,31 @@
+//
+//  boat.swift
+//  swift-boat
+//
+//  Created by Bettina on 7/19/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+class Boat {
+    
+    let name: String
+    var sailors : [String]
+    var maxSpeedKnots: Double
+    var speedKnots: Double = 0
+   
+    
+    init(name: String, sailors: [String], maxSpeedKnots: Double){
+    
+        self.name = name
+        self.sailors = sailors
+        self.maxSpeedKnots = maxSpeedKnots
+    }
+    
+    convenience init(name: String, maxSpeedKnots: Double) {
+        
+        self.init(name: name, sailors: [String](), maxSpeedKnots: maxSpeedKnots)
+        
+       }
+}
